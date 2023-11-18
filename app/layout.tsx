d9582bebd7a5d2d2c66a1d09822204320f39c49e
Jehee Cheon <jeheecheon@gmail.com>
2023-11-18 16:46:35 +0900

    빌드 에러 해결

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,5 @@
-import { Inter } from 'next/font/google';
 import { lusitana } from '@/app/ui/fonts'
 import '@/app/lib/global.css';
-
-export const inter = Inter({ subsets: ['latin'], weight: '100' });
 
 export default function RootLayout({
   children,
