4b994d7bf15477479f39ab243e5157b9fd92609d
Jehee Cheon <jeheecheon@gmail.com>
2023-11-18 15:58:23 +0900

    Add side-nave pannel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+import { Inter } from 'next/font/google';
+import { lusitana } from './lib/fonts';
+import '@/app/lib/global.css';
+
+export const inter = Inter({ subsets: ['latin'], weight: '100' });
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
